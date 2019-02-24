d65df2b9091b10b27b5708c0a900f0710acc287d
bryce <brycekbargar@gmail.com>
2019-02-24 12:53:40 -0600

    Log bitwarden

--- src/install-bitwarden.ps1
+++ src/install-bitwarden.ps1
@@ -1,4 +1,7 @@
+Write-Host "Ensuring bitwarden is installed and configured" -ForegroundColor DarkCyan
+
 & scoop install bitwarden-cli
 & scoop update bitwarden-cli
 & bw login brycekbargar@gmail.com
-& { $env:BW_SESSION=$(bw unlock --raw) }
+
+Write-Host "Bitwarden is installed and configured" -ForegroundColor DarkCyan
