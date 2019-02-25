9d6224e134dbf942b675eff31ffc77210bfb4397
bryce <brycekbargar@gmail.com>
2019-02-25 11:50:46 -0600

    Run the setup scripts

--- src/setup.ps1
+++ src/setup.ps1
@@ -1,1 +1,14 @@
 Write-Host "Starting Setup!" -ForegroundColor DarkGreen
+
+& scoop bucket add extras
+& scoop install sudo
+& scoop update sudo
+
+$src = $PSScriptRoot
+
+& sudo Import-Module (Resolve-Path (Join-Path $src "install-autohotkey.ps1")) -Force
+Import-Module (Resolve-Path (Join-Path $src "install-colemak.ps1")) -Force
+Import-Module (Resolve-Path (Join-Path $src "install-bitwarden.ps1")) -Force
+Import-Module (Resolve-Path (Join-Path $src "setup-profile.ps1")) -Force
+Import-Module (Resolve-Path (Join-Path $src "setup-ssh.ps1")) -Force
+Import-Module (Resolve-Path (Join-Path $src "setup-modules.ps1")) -Force
