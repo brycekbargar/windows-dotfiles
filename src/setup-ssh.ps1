5f5f79069d5f5f2343798d4aeec14e86bf9bd364
bryce <brycekbargar@gmail.com>
2019-02-25 17:21:08 -0600

    Unlock vault before downloading keys

--- src/setup-ssh.ps1
+++ src/setup-ssh.ps1
@@ -8,12 +8,14 @@ Write-Verbose "Creating ~/.ssh directory"
 New-Item ~/.ssh -ItemType Directory -Force | Out-Null
 
 Write-Verbose "Downloading keys from bitwarden"
-(bw list items --folderid 5f327073-d3b6-4940-9783-a9db0022589e | ConvertFrom-Json) |
+$session = bw unlock --raw
+(bw list items --folderid 5f327073-d3b6-4940-9783-a9db0022589e --session $session | ConvertFrom-Json) |
     Where-Object type -EQ 1 |
     Select-Object -Property @{ name='itemid'; expression={ $_.id } } -ExpandProperty attachments |
     ForEach-Object {
         Write-Debug $_.fileName
         (bw get attachment $_.id --output (Join-Path (Resolve-Path "~\.ssh\") $_.fileName) --itemid $_.itemid) | Out-Null
     }
+bw lock
 
 Write-Host "Ssh is enabled and ssh-keys are dowloaded from bitwarden" -ForegroundColor DarkCyan
