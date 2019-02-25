fde4c9acc4c136f20f151d84ee216f85bc14de9d
bryce <brycekbargar@gmail.com>
2019-02-25 13:51:43 -0600

    Add git bash scripts

--- src/setup-git.ps1
+++ src/setup-git.ps1
@@ -9,4 +9,13 @@ Write-Verbose "Setting username"
 & git config --system user.name "bryce"
 & git config --system user.email "brycekbargar@gmail.com"
 
+$currentPath = [Environment]::GetEnvironmentVariable('PATH', 'USER')
+Write-Debug $currentPath
+
+if (-not $currentPath.Contains("dotfiles\git")) {
+    Write-Verbose "Setting path"
+    $git = Resolve-Path([System.IO.Path]::Combine($PSScriptRoot, "..", "dotfiles", "git", "bin"))
+    [Environment]::SetEnvironmentVariable('PATH', "$currentPath;$git" ,'USER')
+}
+
 Write-Host "Gitconfig is linked and user is set" -ForegroundColor DarkCyan
