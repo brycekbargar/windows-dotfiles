d20ec6dfedbe1697f599ffc478985edaa255792a
bryce <brycekbargar@gmail.com>
2019-02-26 11:50:11 -0800

    Create hyper-v folders on install

--- src/setup-hyper-v.ps1
+++ src/setup-hyper-v.ps1
@@ -3,6 +3,9 @@ Write-Host "Ensure hyper-v is enabled for hosts" -ForegroundColor DarkCyan
 Write-Verbose "Enabling hyper-v $env:InstallType"
 if ($env:InstallType -eq "host") {
     & sudo Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+    New-Item "~\hyper-v\isos" -ItemType Directory -Force
+    New-Item "~\hyper-v\disks" -ItemType Directory -Force
+    New-Item "~\hyper-v\machines" -ItemType Directory -Force
 }
 
 Write-Host "Hyper-v is enabled for hosts" -ForegroundColor DarkCyan
