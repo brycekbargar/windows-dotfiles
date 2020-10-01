cc834c96e498c83baaa31a7ba591bfdf20d755be
bryce <brycekbargar@gmail.com>
2020-10-01 16:06:37 -0500

    Stop installing hyper

--- src/setup.ps1
+++ src/setup.ps1
@@ -6,7 +6,6 @@ Import-Module (Resolve-Path (Join-Path $src "setup-scoop.ps1")) -Force
 & sudo Import-Module (Resolve-Path (Join-Path $src "install-autohotkey.ps1")) -Force
 Import-Module (Resolve-Path (Join-Path $src "install-colemak.ps1")) -Force
 Import-Module (Resolve-Path (Join-Path $src "install-bitwarden.ps1")) -Force
-Import-Module (Resolve-Path (Join-Path $src "install-hyper.ps1")) -Force
 Import-Module (Resolve-Path (Join-Path $src "install-vscode.ps1")) -Force
 Import-Module (Resolve-Path (Join-Path $src "install-wsl.ps1")) -Force
 Import-Module (Resolve-Path (Join-Path $src "setup-profile.ps1")) -Force
