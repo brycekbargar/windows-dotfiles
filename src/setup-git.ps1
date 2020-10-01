fa98d9d3d538bdf677d91775be72ea5ee01d8ea6
bryce <brycekbargar@gmail.com>
2020-10-01 12:18:03 -0500

    Remove the differentiation betweeen host/guest and home/work

--- src/setup-git.ps1
+++ src/setup-git.ps1
@@ -5,16 +5,6 @@ $dotfile = [System.IO.Path]::Combine($PSScriptRoot, "..", "dotfiles", "git", ".g
 $config = [System.IO.Path]::Combine($HOME, ".gitconfig")
 & sudo New-Item -Path $config -ItemType SymbolicLink -Value $dotfile -Force
 
-Write-Verbose "Setting username $env:InstallEnv"
-if ($env:InstallEnv -eq "home") {
-    & git config --system user.name "bryce"
-    & git config --system user.email "brycekbargar@gmail.com"
-}
-elseif ($env:InstallEnv -eq "milyli") {
-    & git config --system user.name "katherine"
-    & git config --system user.email "katherine@milyli.com"
-}
-
 $currentPath = [Environment]::GetEnvironmentVariable('PATH', 'USER')
 Write-Debug $currentPath
 
--- src/setup.ps1
+++ src/setup.ps1
@@ -1,17 +1,4 @@
-param(
-    [Parameter(mandatory=$true)]
-    [ValidateSet('home', 'milyli')]
-    [String]$InstallEnv,
-
-    [Parameter(mandatory=$true)]
-    [ValidateSet('host', 'guest')]
-    [String]$InstallType
-)
-
 Write-Host "Starting Setup!" -ForegroundColor DarkGreen
-
-$env:InstallEnv = $InstallEnv
-$env:InstallType = $InstallType
 
 $src = $PSScriptRoot
 
@@ -25,4 +12,3 @@ Import-Module (Resolve-Path (Join-Path $src "setup-profile.ps1")) -Force
 Import-Module (Resolve-Path (Join-Path $src "setup-ssh.ps1")) -Force
 Import-Module (Resolve-Path (Join-Path $src "setup-modules.ps1")) -Force
 Import-Module (Resolve-Path (Join-Path $src "setup-git.ps1")) -Force
-Import-Module (Resolve-Path (Join-Path $src "setup-hyper-v.ps1")) -Force
