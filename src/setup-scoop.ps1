192ba87f973b1a96c16ab52990d0d34b82aa537f
bryce <brycekbargar@gmail.com>
2020-10-01 13:01:19 -0500

    Annouce what is happening during scoop setup

--- src/setup-scoop.ps1
+++ src/setup-scoop.ps1
@@ -1,14 +1,22 @@
+Write-Host "Ensure scoop is setup and happy" -ForegroundColor DarkCyan
+
+Write-Verbose "Installing latest scoop and related tools"
 & scoop bucket add extras
+& scoop update
+& scoop install 7zip
+& scoop update 7zip
 & scoop install sudo
 & scoop update sudo
 
-# Scoop Checkup fixes
-    # 'Inno Setup Unpacker' is not installed! It's required for unpacking InnoSetup files.
-    & scoop install innounp
+Write-Verbose "Fixing scoop checkup issues"
+Write-Verbose "Inno Setup Unpacker' is not installed! It's required for unpacking InnoSetup files."
+& scoop install innounp
 
-    # Windows Defender may slow down or disrupt installs with realtime scanning.
-    & sudo Add-MpPreference -ExclusionPath "$HOME\scoop"
-    & sudo Add-MpPreference -ExclusionPath 'C:\ProgramData\scoop'
+Write-Verbose "Windows Defender may slow down or disrupt installs with realtime scanning."
+& sudo Add-MpPreference -ExclusionPath "$HOME\scoop"
+& sudo Add-MpPreference -ExclusionPath 'C:\ProgramData\scoop'
 
-    # LongPaths support is not enabled.
-    & sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+Write-Verbose "LongPaths support is not enabled."
+& sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+
+Write-Host "Scoop is setup and happy" -ForegroundColor DarkCyan
