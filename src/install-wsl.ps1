096e025e472e1709806970b5121cc6e7d4768a7c
bryce <brycekbargar@gmail.com>
2020-10-01 16:32:05 -0500

    Link the settings json for the new terminal

--- src/install-wsl.ps1
+++ src/install-wsl.ps1
@@ -6,10 +6,17 @@ Write-Verbose "Installing terminal and vcredist2019"
 & scoop install extras/vcredist2019
 & scoop update extras/vcredist2019
 
-Write-Verbose "Enabling WSL"
-& sudo dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-& sudo dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-Write-Warning "WSL is enabled, a restart may be required"
+Write-Verbose "Linking configuration"
+$dotfile = [System.IO.Path]::Combine($PSScriptRoot, "..", "dotfiles", "windows-terminal", "settings.json")
+$config = [System.IO.Path]::Combine("$env:LOCALAPPDATA", "Microsoft", "Windows Terminal", "settings.json")
+& sudo New-Item -Path "$config" -ItemType SymbolicLink -Value "$dotfile" -Force
+
+if(-not (& where.exe wsl)) {
+    Write-Verbose "Enabling WSL"
+    & sudo dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+    & sudo dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+    Write-Warning "WSL is enabled, a restart may be required"
+}
 
 $wsl_update_x64 = "$HOME\Downloads\wsl_update_x64.msi"
 if (-not (Test-Path $wsl_update_x64)) {
