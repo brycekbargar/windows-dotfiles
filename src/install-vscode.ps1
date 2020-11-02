46b9727064058c3e91ad6e62357ac8d2cc421dc1
bryce <brycekbargar@gmail.com>
2020-11-02 17:31:55 -0600

    Remove vscode extension and settings setup

--- src/install-vscode.ps1
+++ src/install-vscode.ps1
@@ -7,17 +7,7 @@ Write-Verbose "Installing vscode"
 Write-Verbose "Creating context menu entry"
 & "$HOME\scoop\apps\vscode\current\vscode-install-context.reg"
 
-Write-Verbose "Installing extensions"
-& code --install-extension vscodevim.vim
-& code --install-extension dracula-theme.theme-dracula
-& code --install-extension CoenraadS.bracket-pair-colorizer
-& code --install-extension ms-mssql.mssql
-
-Write-Verbose "Linking configuration"
-$dotfile = [System.IO.Path]::Combine($PSScriptRoot, "..", "dotfiles", "vscode", "settings.json")
-$config = [System.IO.Path]::Combine("$env:APPDATA", "Code", "User", "settings.json")
-& sudo New-Item -Path $config -ItemType SymbolicLink -Value $dotfile -Force
-
+Write-Verbose "Installing Cascadia-Code"
 & sudo scoop install Cascadia-Code 
 & sudo scoop update Cascadia-Code 
 
