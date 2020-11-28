d0a0e70e74ca1bab69733c4456c90e32e7e4c871
bryce <brycekbargar@gmail.com>
2020-11-28 15:14:56 -0600

    Fix the wslcode command to work with wsl2

--- dotfiles/modules/VisualStudioCode/VisualStudioCode.psd1
+++ dotfiles/modules/VisualStudioCode/VisualStudioCode.psd1
@@ -1,6 +1,6 @@
 @{
     RootModule = 'VisualStudioCode.psm1';
-    ModuleVersion = '1.0.0.0';
+    ModuleVersion = '2.0.0.0';
     GUID = 'da9c7490-199d-415c-9f15-4171bdfc2d58';
     Description = 'Additional functionality for vscode.';
     PowerShellVersion = '7.0';
--- dotfiles/modules/VisualStudioCode/VisualStudioCode.psm1
+++ dotfiles/modules/VisualStudioCode/VisualStudioCode.psm1
@@ -8,8 +8,11 @@ Starts a vscode instance using the Remote Session extension.
 Turns the absolute path into a relative path in the Remote.
 Calls `code` with appropriate parameters to start in a remote session.
 
-.Parameter Name
-The full path (or relative path from the root of wsl) of the folder/file to edit in vscode.
+Note: This assumes the windows and wsl user are the same name.
+
+.Parameter Path
+The path (from the _src folder) of the folder/file to edit in vscode.
+Alternatively, the full path if it starts with '/'
 
 #>
 function wslcode {
@@ -17,6 +20,11 @@ function wslcode {
         [String]$Path
     )
 
-    $debianPath = $Path.Replace("$WSLROOT", '').Replace('\', '/')
-    & code --remote wsl+Debian $debianPath
+    $Path = $Path.Replace("\", "/")
+
+    if (-Not $Path.StartsWith("/")) {
+        $Path = "/home/$env:USERNAME/_src/$Path"
+    }
+
+    & code --remote wsl+Debian "$Path"
 }
